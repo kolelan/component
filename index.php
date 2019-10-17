ec27933398c18bf377acd371ed7fa47eb9db983a
Kolelan <kolelan@yandex.ru>
2019-10-17 23:41:51 +0300

    Second version

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 error_reporting(E_ALL);
+use app\commands\HelloCommand;
 use Symfony\Component\Console\Application;
 require __DIR__.'/vendor/autoload.php';
 $app = new Application();
+$app->add(new HelloCommand());
 $app->run();
